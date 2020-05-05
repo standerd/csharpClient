cbd65496daf4696972d50723babfbe06b0a04cd3
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2020-05-05 16:07:19 +0200

    Image Uplod Done

---
+++ src/features/profiles/ProfilePhotos.tsx
@@ -0,0 +1,92 @@
+import React, { useContext, useState } from "react";
+import { Tab, Header, Card, Image, Button, Grid } from "semantic-ui-react";
+import { RootStoreContext } from "../../app/stores/rootStore";
+import PhotoUploadWidget from "../../app/common/photoUpload/PhotoUploadWidget";
+import { observer } from "mobx-react-lite";
+
+const ProfilePhotos = () => {
+  const rootStore = useContext(RootStoreContext);
+  const {
+    profile,
+    isCurrentuser,
+    uploadPhoto,
+    uploadingPhoto,
+    setMainPhoto,
+    loading,
+    deletePhoto,
+  } = rootStore.profileStore;
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+  const [target, setTarget] = useState<string | undefined>(undefined);
+  const [deleteTarget, setDeleteTarget] = useState<string | undefined>(
+    undefined
+  );
+
+  const handleUploadImage = (photo: Blob) => {
+    uploadPhoto(photo).then(() => setAddPhotoMode(false));
+  };
+
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column style={{ paddingBottom: 0 }} width={16}>
+          <Header icon="image" content="Photos" floated="left" />
+          {isCurrentuser && (
+            <Button
+              floated="right"
+              basic
+              content={addPhotoMode ? "Cancel" : "Add Photo"}
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <PhotoUploadWidget
+              uploadPhoto={handleUploadImage}
+              loading={uploadingPhoto}
+            />
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile &&
+                profile.photos.map((photo) => (
+                  <Card key={photo.id}>
+                    <Image src={photo.url} />
+                    {isCurrentuser && (
+                      <Button.Group fluid widths={2}>
+                        <Button
+                          name={photo.id}
+                          onClick={(e) => {
+                            setMainPhoto(photo);
+                            setTarget(e.currentTarget.name);
+                          }}
+                          loading={loading && target === photo.id}
+                          disabled={photo.isMain}
+                          basic
+                          positive
+                          content="Main"
+                        />
+                        <Button
+                          name={photo.id}
+                          disabled={photo.isMain}
+                          onClick={(e) => {
+                            deletePhoto(photo);
+                            setDeleteTarget(e.currentTarget.name);
+                          }}
+                          loading={loading && deleteTarget === photo.id}
+                          basic
+                          negative
+                          icon="trash"
+                        />
+                      </Button.Group>
+                    )}
+                  </Card>
+                ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
+
+export default observer(ProfilePhotos);
